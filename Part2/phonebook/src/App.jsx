cfd686bdf851d7d5a46e26fe10d4a617ee4b83b3
RKP1202 <b22es003@iitj.ac.in>
2024-12-11 19:07:40 +0530

    Completed The Phonebook Step 6

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,24 +1,24 @@
-import { use } from 'react'
+import { use, useEffect } from 'react'
 import { useState } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
+
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '8262929464',
-      id: 1
-    },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchValue, setSearchValue] = useState('');
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      },)
+  } , [])
   // console.log(typeof(persons[0].number));
 
   const handleSubmit = (event) => {
